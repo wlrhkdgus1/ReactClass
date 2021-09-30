951c2a7f25795047bc77cc15b7224c8004566ed7
Kwang Hyun Ji <gwgw4578@gmail.com>
2021-09-30 18:15:17 +0900

    21 09 30 react blog

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,22 +1,88 @@
+/* eslint-disable */
+
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
 
+  let [글제목, 글제목변경] = useState(['남자 코트 추천','강남 우동 맛집','리액트 독학']);
+  let [따봉, 따봉변경] = useState(0);
+  
+  let [modal,modal변경] = useState(false);
+ 
   let posts = '강남 고기 맛집';
+
+  function 제목바꾸기(){
+    var newArray = [...글제목];
+    newArray[0] = '여자코트 추천';
+    글제목변경( newArray );
+  }
 
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
+      <button onClick={ 제목바꾸기 } >버튼</button>
       <div className="list">
-        <h3> { posts } </h3>
-        <p>9월 15일 발행</p>
+      <h3> { 글제목[0] } <span onClick={ ()=>{ 따봉변경(따봉+ 1) } } >👍</span> { 따봉 }</h3>
+        <p>10월 1일 발행</p>
         <hr/>
       </div>
+      <div className="list">
+        <h3> { 글제목[1] } </h3>
+        <p>10월 2일 발행</p>
+        <hr/>
+        </div>
+        <div className="list">
+        <h3 onClick={ ()=>{ modal변경(true) } } > { 글제목[2] } </h3>
+        <p>10월 3일 발행</p>
+        <hr/>
+        </div>
+
+        {
+
+          글제목.map(function(a){
+            return (
+            <div className="list">
+              <h3> { a } </h3>
+              <p>10월 2일 발행</p>
+              <hr/>
+           </div>
+             )
+          })
+
+        }
+
+
+
+
+
+
+
+
+        <button onClick={ ()=> { modal변경(!modal)}}> 열고닫는버튼</button>
+      {
+        modal === true
+        ? <Modal></Modal>
+        : null
+      }
+     
     </div>
   );
 }
+
+function Modal(){
+  return(
+  <div className="modal1">
+     <h2>제목</h2>
+     <p>날짜</p>
+     <p>상세내용</p>
+  </div>
+  )
+}
+
+
 
 export default App;
