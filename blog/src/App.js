a3b5c3254d4ad35a2041472a0af92bea12245a57
Kwang Hyun Ji <gwgw4578@gmail.com>
2021-10-02 16:22:26 +0900

    21 10 02 props study

--- blog/src/App.js
+++ blog/src/App.js
@@ -8,8 +8,8 @@ function App() {
 
   let [글제목, 글제목변경] = useState(['남자 코트 추천','강남 우동 맛집','리액트 독학']);
   let [따봉, 따봉변경] = useState(0);
-  
   let [modal,modal변경] = useState(false);
+  let [누른제목, 누른제목변경] = useState(0);
  
   let posts = '강남 고기 맛집';
 
@@ -24,48 +24,27 @@ function App() {
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
-      <button onClick={ 제목바꾸기 } >버튼</button>
-      <div className="list">
-      <h3> { 글제목[0] } <span onClick={ ()=>{ 따봉변경(따봉+ 1) } } >👍</span> { 따봉 }</h3>
-        <p>10월 1일 발행</p>
-        <hr/>
-      </div>
-      <div className="list">
-        <h3> { 글제목[1] } </h3>
-        <p>10월 2일 발행</p>
-        <hr/>
-        </div>
-        <div className="list">
-        <h3 onClick={ ()=>{ modal변경(true) } } > { 글제목[2] } </h3>
-        <p>10월 3일 발행</p>
-        <hr/>
-        </div>
-
-        {
-
-          글제목.map(function(a){
+         {
+          글제목.map(function(a , i){
             return (
             <div className="list">
-              <h3> { a } </h3>
+              <h3 onClick={ ()=>{ 누른제목변경(i) } }> { a } <span onClick={ ()=>{ 따봉변경(따봉+1) } }>👍</span>
+              {따봉}</h3>
               <p>10월 2일 발행</p>
               <hr/>
            </div>
              )
           })
+        } 
 
-        }
-
-
-
-
-
-
-
+        {/* <button onClick={ ()=>{ 누른제목변경(0) } }>버튼1</button>
+        <button onClick={ ()=>{ 누른제목변경(1) } }>버튼2</button>
+        <button onClick={ ()=>{ 누른제목변경(2) } }>버튼3</button> */}
 
         <button onClick={ ()=> { modal변경(!modal)}}> 열고닫는버튼</button>
       {
         modal === true
-        ? <Modal></Modal>
+        ? <Modal 글제목={글제목} 누른제목={누른제목} ></Modal>
         : null
       }
      
@@ -73,10 +52,10 @@ function App() {
   );
 }
 
-function Modal(){
+function Modal(props){
   return(
   <div className="modal1">
-     <h2>제목</h2>
+     <h2>{ props.글제목[props.누른제목] }</h2>
      <p>날짜</p>
      <p>상세내용</p>
   </div>
